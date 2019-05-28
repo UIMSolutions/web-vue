4cf3e54ef88ba739d0e6ac688b7c957a98407805
uim <ui-manufaktur@gmail.com>
2019-05-28 16:45:31 +0200

    Little changes

--- source/uim/vue/obj.d
+++ source/uim/vue/obj.d
@@ -4,17 +4,27 @@ import uim.vue;
 
 class DVUEObj {
 	this() {}
+	this(string aName) { this; _name = aName; }
 
 	mixin(TProperty!("string", "name"));
-	mixin(TPropertyAA!("string", "string", "data"));
-	mixin(TPropertyAA!("string", "string", "filters"));
-	mixin(TPropertyAA!("string", "string", "methods"));
-	mixin(TPropertyAA!("string", "string", "computed"));
-	mixin(TPropertyAA!("string", "string", "watch"));
 
-	override string toString() {
-		return "";
-	}
+	mixin(TPropertyAA!("string", "string", "data"));
+	O data(string name, string value) { _data[name] = value; return cast(O)this; }
+	O data(string name, T value) { _data[name] = to!string(value); return cast(O)this; }
+
+	mixin(TPropertyAA!("string", "string", "filters"));
+	O filter(string name, string value) { _filters[name] = value; return cast(O)this; }
+
+	mixin(TPropertyAA!("string", "string", "methods"));
+	O method(string name, string value) { _methods[name] = value; return cast(O)this; }
+
+	mixin(TPropertyAA!("string", "string", "computed"));
+	O computed(string name, string value) { _computed[name] = value; return cast(O)this; }
+
+	mixin(TPropertyAA!("string", "string", "watch"));
+	O watch(string name, string value) { _watch[name] = value; return cast(O)this; }
+
+	override string toString() { return ""; }
 }
 auto VUEObj() { return new DVUEObj(); }
 
